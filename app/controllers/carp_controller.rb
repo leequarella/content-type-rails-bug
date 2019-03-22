6510caebd618bd6985fa70901f23fee36ad4bc99
Lee Quarella <leequarella@gmail.com>
2019-03-22 15:12:19 -0400

    Remove unecessary status

--- app/controllers/carp_controller.rb
+++ app/controllers/carp_controller.rb
@@ -1,7 +1,6 @@
 class CarpController < ApplicationController
   def index
     render body: "test content",
-      status: 422,
       content_type: "text/html; sandwiches=tasty; charset=utf-8; birds=flying"
   end
 end
